258f2d0c4e9a1638cf1c634e8517baa0efd82ddc
reginacuisine <reginatonde44@gmail.com>
2026-01-31 17:38:14 +0000

    Activation des notifications par mail

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Configuration pour recevoir les alertes par mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'reginatonde44@gmail.com'
+EMAIL_HOST_PASSWORD = 'mzma efmp aasw lbjo'
